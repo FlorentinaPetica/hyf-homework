48dbf25d880e43b17c14ca42c71313badc359b3e
FlorentinaPetica <florentina.petica@gmail.com>
2021-06-28 20:06:39 +0200

    File task1 - age-ify was added

---
+++ javascript/javascript1/week1/task1.js
@@ -0,0 +1,11 @@
+console.log("Age-ify!") // Age-ify (A future age calculator)
+
+const yearOfBirth = 1986;
+
+let yearFuture = 2045;
+
+yearFuture = 2027;
+
+let age = yearFuture - yearOfBirth;
+
+console.log("You will be " + age + " years old in " + yearFuture)
