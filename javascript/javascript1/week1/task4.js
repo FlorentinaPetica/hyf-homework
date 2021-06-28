a8d31d79062f14c29d07e8d58188dae59f078b6c
FlorentinaPetica <florentina.petica@gmail.com>
2021-06-28 22:02:28 +0200

    File task4 - startup name was added

---
+++ javascript/javascript1/week1/task4.js
@@ -0,0 +1,11 @@
+console.log('Ez Namey!') // Ez Namey (Startup name generator)
+
+let firstWords = ['Amusing', 'Enjoyable', 'Entertaining', 'Lively', 'Pleasant', 'Boisterous', 'Convivial', 'Merry', 'Hyggelig', 'Zippy']
+
+let secondWords = ['Gargle', 'Thingy', 'Master', 'Socks', 'Whiskers', 'Cake', 'Fault', 'Ideas', 'Lettuce', 'Story'];
+
+const randomNumber = Math.floor(Math.random() * 10);
+
+let startUpName = firstWords[randomNumber] + ' ' + secondWords[randomNumber];
+
+console.log('Your start ups new name is: \''+ startUpName + '\' and contains ' + startUpName.length + ' character.')
