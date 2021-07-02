388b5718327889a1886b7397ba6623a87d1b2de9
FlorentinaPetica <florentina.petica@gmail.com>
2021-07-02 20:12:37 +0200

    Erased re-assigned value for yearFuture

--- javascript/javascript1/week1/task1.js
+++ javascript/javascript1/week1/task1.js
@@ -2,10 +2,8 @@ console.log("Age-ify!") // Age-ify (A future age calculator)
 
 const yearOfBirth = 1986;
 
-let yearFuture = 2045;
-
-yearFuture = 2027;
+const yearFuture = 2045;
 
 let age = yearFuture - yearOfBirth;
 
-console.log("You will be " + age + " years old in " + yearFuture)
+console.log(`You will be ${age} years old in ${yearFuture}`)
