71ea4bdf6ab2da7ce77b8fb7f69f02c4f17efe53
FlorentinaPetica <florentina.petica@gmail.com>
2021-07-02 20:15:15 +0200

    Changed let to const

--- javascript/javascript1/week1/task3.js
+++ javascript/javascript1/week1/task3.js
@@ -1,6 +1,6 @@
 console.log('Housey pricey!') // Housey pricey (A house price estimator)
 
-let peter = {
+const peter = {
     houseWidth: 8,
     houseDepth: 10,
     houseHight: 10,
@@ -8,9 +8,9 @@ let peter = {
     houseCost: 2500000
 }
 
-let volumeInMetersPeter = peter.houseWidth * peter.houseDepth * peter.houseHight;
+const volumeInMetersPeter = peter.houseWidth * peter.houseDepth * peter.houseHight;
 
-let housePricePeter = volumeInMetersPeter * 2.5 * 1000 + peter.gardenSizeInM2 * 300;
+const housePricePeter = volumeInMetersPeter * 2.5 * 1000 + peter.gardenSizeInM2 * 300;
 
 if (housePricePeter < peter.houseCost) {
     console.log('Peter pays more than the market value of the house.')
@@ -18,7 +18,7 @@ if (housePricePeter < peter.houseCost) {
     console.log('Peter pays less than the market value of the house.')
 }
 
-let julia = {
+const julia = {
     houseWidth: 5,
     houseDepth: 11,
     houseHight: 8,
@@ -26,9 +26,9 @@ let julia = {
     houseCost: 1000000
 }
 
-let volumeInMetersJulia = julia.houseWidth * julia.houseDepth * julia.houseHight;
+const volumeInMetersJulia = julia.houseWidth * julia.houseDepth * julia.houseHight;
 
-let housePriceJulia = volumeInMetersJulia * 2.5 * 1000 + julia.gardenSizeInM2 * 300;
+const housePriceJulia = volumeInMetersJulia * 2.5 * 1000 + julia.gardenSizeInM2 * 300;
 
 if (housePriceJulia < julia.houseCost) {
     console.log('Julia pays more than the market value of the house.')
