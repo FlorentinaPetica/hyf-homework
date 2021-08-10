c8bc63153cea295278c199f0bc3425eb0506e2af
FlorentinaPetica <florentina.petica@gmail.com>
2021-08-10 21:55:26 +0200

    Find and count the danish letters

--- javascript/javascript2/week1/js2w1.js
+++ javascript/javascript2/week1/js2w1.js
@@ -19,3 +19,32 @@ function findShortWord(danishWords) {
 }
 
 console.log(findShortWord(danishWords))
+
+// find and count the danish letters
+
+const danishString = "Jeg har en blå bil";
+const danishString2 = "Blå grød med røde bær";
+
+function countDanishLetters(str) {
+
+    const arrStr = str.toLowerCase().split('')
+    const danishCharacters = [] //find all danish characters in the string
+    for (let i = 0; i < arrStr.length; i++) {
+        if (arrStr[i]==='ø' || arrStr[i]==='å' || arrStr[i]==='æ') {
+            danishCharacters.push(arrStr[i])
+        }
+    }
+
+    let result = {total: danishCharacters.length}
+    danishCharacters.forEach(item => {
+        if(result[item]) {
+            result[item] +=1
+            return
+        }
+        result[item] = 1;
+    })
+    return result
+}
+
+console.log(countDanishLetters(danishString));
+console.log(countDanishLetters(danishString2));
