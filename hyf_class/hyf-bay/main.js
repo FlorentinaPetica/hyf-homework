b2b2daaff06ad98ff1f30193e7ea140cdcf8eefe
FlorentinaPetica <florentina.petica@gmail.com>
2021-08-11 20:15:16 +0200

    hyf-bay changd according to hyf solution

--- hyf_class/hyf-bay/main.js
+++ hyf_class/hyf-bay/main.js
@@ -3,24 +3,20 @@ console.log("Script loaded");
 const products = getAvailableProducts();
 console.log(products);
 
-let element = document.querySelector('ul')
+let element = document.getElementById('list')
 
 function renderProducts(products) {
-    for (let i=0; i<products.length; i++) {
-        let list = document.createElement('li')
+
+    for (let i=1; i<products.length; i++) {
+
+        let list = document.createElement('ul')
+        list.innerHTML =  `
+        <li>${products[i].name}</li>
+        <li>Price: ${products[i].price}</li>
+        <li>Ratings: ${products[i].rating}</li>`;
         element.appendChild(list);
-        list.innerHTML = products[i].name
-        list.addEventListener('click', openInfo)
-        function openInfo() {
-            let price = document.createElement('span')
-            let ratings = document.createElement('span')
-            price.innerHTML = `Price: ${products[i].price}`
-            ratings.innerHTML = `Rating: ${products[i].rating}`
-            list.append(price);
-            list.append(ratings);
-            list.removeEventListener('click', openInfo)
-        }
     }
+
 }
 
 renderProducts(products); // This should create the ul and the li's with the individual products details
