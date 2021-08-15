36f89a5848006d719c0cda0c510f43bbb4b9c1af
FlorentinaPetica <florentina.petica@gmail.com>
2021-08-15 18:24:10 +0200

    doubling of number

---
+++ javascript/javascript2/week2/hyf-bay/js2w2.js
@@ -0,0 +1,8 @@
+// Doubling of number
+
+let numbers = [1, 2, 3, 4];
+
+let newNumbers = numbers.filter((number) => number % 2 !== 0)
+                        .map((number) => number * 2)
+
+console.log("The doubled numbers are", newNumbers);
