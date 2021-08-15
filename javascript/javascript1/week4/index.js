131bf8fdd819a6ec5585206cdf4cb5eafec05d8e
FlorentinaPetica <florentina.petica@gmail.com>
2021-08-15 16:35:23 +0200

    changing the command to lower case

--- javascript/javascript1/week4/index.js
+++ javascript/javascript1/week4/index.js
@@ -3,6 +3,8 @@ const todoList = []
 const calculation = ['+', '-', '/', '*']
 
 function getReply(command) {
+    const command_ = command.toLowerCase()
+
     if (command.indexOf('name is') !== -1) {
         names.push(command.slice(command.indexOf('name is') + 'name is'.length))
         return `Nice to meet you ${names[0]}`
@@ -12,7 +14,7 @@ function getReply(command) {
         return `Your name is ${names[0]}`
     }
 
-    if (command.toLowerCase().indexOf('add') !== -1) {
+    if (command_.indexOf('add') !== -1) {
         todoList.push(
             command.slice(command.indexOf('add') + 5, command.indexOf('to') - 1)
         )
@@ -21,13 +23,13 @@ function getReply(command) {
     for (let i = 0; i < todoList.length; i++) {
         if (
             command.includes(todoList[i]) &&
-            command.toLowerCase().indexOf('add') !== -1
+            command_.indexOf('add') !== -1
         ) {
             return `${todoList[i]} added to your todo list`
         }
     }
 
-    if (command.toLowerCase().includes('remove')) {
+    if (command_.includes('remove')) {
         for (let i = 0; i < todoList.length; i++) {
             if (command.includes(todoList[i])) {
                 let removedItem = todoList.splice(
@@ -39,7 +41,7 @@ function getReply(command) {
         }
     }
 
-    if (command.toLowerCase().includes('What is on my')) {
+    if (command_.includes('what is on my')) {
         return `You have ${todoList} in your todo list`
     }
 
@@ -69,7 +71,7 @@ function getReply(command) {
         }
     }
 
-    if (command.toLowerCase().includes('minutes')) {
+    if (command_.includes('minutes')) {
         const totalMinutes = command.match(/\d+/)
         const totalInMilliseconds = totalMinutes[0] * 60 * 1000
         console.log(`Timer set to ${totalMinutes[0]} minutes.`)
@@ -78,7 +80,7 @@ function getReply(command) {
             console.log(minutes + ' minut(s) are done')
         }
 
-        setTimeout(setTimer, totalInMilliseconds, totalMinutes)
+        // setTimeout(setTimer, totalInMilliseconds, totalMinutes)
         
     } else {
         return `Can you please repeat?`
