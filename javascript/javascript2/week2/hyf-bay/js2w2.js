ebf0f5547507011f3c75cf463567d51c4e250329
FlorentinaPetica <florentina.petica@gmail.com>
2021-08-28 16:36:17 +0200

    Changed let to const and adding console.log

--- javascript/javascript2/week2/hyf-bay/js2w2.js
+++ javascript/javascript2/week2/hyf-bay/js2w2.js
@@ -15,6 +15,7 @@ console.log(longTitles)
 
 // number of movies made between 1980-1989 (including both the years)
 const numberOfMovies = movies.filter((movie) => movie.year >= 1980 && movie.year <= 1989).length
+console.log(numberOfMovies)
 
 // extra key called tag. The tag is based on the rating: Good (>= 7), Average (>= 4 and < 7), Bad (< 4)
 const tagMovies = movies.map((movie) => {
@@ -28,24 +29,30 @@ const tagMovies = movies.map((movie) => {
         movie['tag'] = 'Bad'
     } return movie
 })
+console.log(tagMovies)
 
 // Using chaining, first filter the movies array to only contain the movies rated higher than 6. Now map the movies array to only the rating 
 const chainMovies = movies.filter((movie) => movie.rating > 6)
                           .map((movie) => movie.rating).sort(function(a, b){return b - a})
+console.log(chainMovies)
 
 // Count the total number of movies containing any of following keywords: Surfer, Alien or Benjamin
 const keywordsMovies = movies.filter((movie) => movie.title.includes('Surfer') || movie.title.includes('Alien') || movie.title.includes('Benjamin')).length
+console.log(keywordsMovies)
 
 // Create an array of movies where a word in the title is duplicated
-let duplicatedTitles = movies.filter(movie => {
-    let splitArray = movie.title.toLowerCase().split(" ");
-    let duplicatedWords = splitArray.filter((item, index) => {
+const duplicatedTitles = movies.filter(movie => {
+    const splitArray = movie.title.toLowerCase().split(" ");
+    const duplicatedWords = splitArray.filter((item, index) => {
         return splitArray.indexOf(item) !== index
     })
     return duplicatedWords.length
 })
+console.log(duplicatedTitles)
+
 // Calculate the average rating of all the movies using reduce. Optional
 const averageRating = (movies.map((movie) => movie.rating).reduce((total, value) => total + value, 0)/movies.length).toFixed(2)
+console.log(averageRating)
 
 // Count the total number of Good, Average and Bad movies using reduce, return {goodMovies: 33, averageMovies: 45, goodMovies: 123}
 const reduceTag = movies.reduce(
