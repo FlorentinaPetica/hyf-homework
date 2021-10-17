02cba249e03ae92434e838bf0826946bf64369b2
FlorentinaPetica <florentina.petica@gmail.com>
2021-10-17 18:41:48 +0200

    In class exercise-template

--- nodejs/week2/exercise-template/src/backend/api/movies-router.js
+++ nodejs/week2/exercise-template/src/backend/api/movies-router.js
@@ -5,7 +5,29 @@ const movies = require("../data/movies.json");
 
 router.get("/", async (request, response) => {
   console.log(movies);
-  response.send({ data: [] });
+  response.send({ data: [movies] });
+});
+
+router.get("/:id", async (request, response) => {
+  const movieid = Number(request.params.id)
+  console.log(movieid);
+  const moviereguest = movies.filter(movie => movie.id === movieid)
+  
+  response.send({ data: [moviereguest] });
+});
+
+router.get("/", async (request, response) => {
+  const {beginYear, endYear} = request.query
+  const filteredMovies = movies.filter(movie => movie.year > parseInt(beginYear) && movie.year < parseInt(endYear))
+
+  response.json({ data: [filteredMovies] });
+});
+
+router.get("/", async (request, response) => {
+  const {beginYear, endYear, minRating} = request.query
+  const filteredMovies = movies.filter(movie => movie.year > parseInt(beginYear) && movie.year < parseInt(endYear) && movie.rating >= parseInt(minRating))
+
+  response.json({ data: [filteredMovies] });
 });
 
 module.exports = router;
