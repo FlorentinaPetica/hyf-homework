f57b5fbe5c93350e3e3fe85b58152228a9518941
FlorentinaPetica <florentina.petica@gmail.com>
2021-10-24 19:24:15 +0200

    In class exercises

--- nodejs/week3/exercise-template/src/backend/api/concerts.js
+++ nodejs/week3/exercise-template/src/backend/api/concerts.js
@@ -12,4 +12,36 @@ router.get("/", async (request, response) => {
   }
 });
 
+
+router.post("/", async (request, response) => {
+  try {
+    //Validation
+    if (!!success[0]) {
+      const success = await knex('concerts').insert(request.body)
+      response.status(200).json(success)
+    } else {
+      response.status(500).end()
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
+router.patch("/:id", async (request, response) => {
+  try {
+    const concertsId = await knex("concerts").where({id:request.params.id}).update(request.body, [...Object.keys(request.body)]);
+    response.status(200).json(concertsId);
+  } catch (error) {
+    throw error;
+  }
+});
+
+router.delete("/:id", async (request, response) => {
+  try {
+    const concertsId = await knex("concerts").where({id:request.params.id}).del();
+    response.status(200).json(concertsId);
+  } catch (error) {
+    throw error;
+  }
+});
 module.exports = router;
