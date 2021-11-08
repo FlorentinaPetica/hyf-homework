50c2cb82deb2487b91064101725706f5402e01d3
FlorentinaPetica <florentina.petica@gmail.com>
2021-11-08 19:15:01 +0100

    Post requests modified

--- nodejs/week3/homework/warmup/index.js
+++ nodejs/week3/homework/warmup/index.js
@@ -1,6 +1,10 @@
 const { json } = require("express");
 const express = require("express");
 const app = express();
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
@@ -29,26 +33,25 @@ app.get("/calculator/division", (req, res) => {
 })
 
 app.post("/calculator/add", (req, res) => {
-    console.log(req.query);
-    const requestedNum = Object.values(req.query).map(num => Number(num))
+    const requestedNum = Object.values(req.body).map(num => Number(num))
     const addition = requestedNum.reduce((previousValue, currentValue) => previousValue + currentValue)
     res.send({result: `${addition}`})
 })
 
 app.post("/calculator/subtraction", (req, res) => {
-    const requestedNum = Object.values(req.query).map(num => Number(num))
+    const requestedNum = Object.values(req.body).map(num => Number(num))
     const subtraction = requestedNum.reduce((previousValue, currentValue) => previousValue - currentValue)
     res.send({result: `${subtraction}`})
 })
 
 app.post("/calculator/multiply", (req, res) => {
-    const requestedNum = Object.values(req.query).map(num => Number(num))
+    const requestedNum = Object.values(req.body).map(num => Number(num))
     const multiply = requestedNum.reduce((previousValue, currentValue) => previousValue * currentValue)
     res.send({result: `${multiply}`})
 })
 
 app.post("/calculator/division", (req, res) => {
-    const requestedNum = Object.values(req.query).map(num => Number(num))
+    const requestedNum = Object.values(req.body).map(num => Number(num))
     const division = requestedNum.reduce((previousValue, currentValue) => previousValue / currentValue)
     res.send({result: `${division}`})
 })
